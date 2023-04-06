16eeab18ccb91f43ad51c7b1cabd05469999e038
Oliver Coates <oco36@cs23024jv.canterbury.ac.nz>
2023-04-07 10:43:34 +1200

    Preparations for merge, Fixes to testing

---
+++ Eclipse Workspace/UltimateEarthChampionship/src/champion/champions/AdamSmith.java
@@ -0,0 +1,22 @@
+package champion.champions;
+
+import champion.Champion;
+
+public class AdamSmith extends Champion{
+
+	public static String name = "Adam Smith";
+	
+	// Stat Boosts:
+	private static int healthBoost = -1;
+	private static int staminaBoost = 1;
+	private static int offenseBoost = 1;
+	private static int defenseBoost = 0;
+	
+	// Price
+	private static float startingPrice = 50f;
+	private static float priceChangeWeekly = 1.1f;
+	
+	public AdamSmith() {
+		super(name, healthBoost, staminaBoost, offenseBoost, defenseBoost, startingPrice, priceChangeWeekly);
+	}
+}
