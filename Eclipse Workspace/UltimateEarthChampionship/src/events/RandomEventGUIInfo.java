141f0271bf014699619f538cd3825c505eee6031
Oliver Coates <oco36@uclive.ac.nz>
2023-04-12 17:53:35 +1200

    Random events and Game environment class added. These are both NOT TESTED as functionality is limited until team class has been merged to main

---
+++ Eclipse Workspace/UltimateEarthChampionship/src/events/RandomEventGUIInfo.java
@@ -0,0 +1,38 @@
+package events;
+
+/**
+ * Random Event GUI Info
+ * This class is intended to act as a 'parcel' of information to be instantiated at each random event and passed through to the GUI
+ * It is done like this so that random events can be easily stored in a list and presented to the user once at a time by the GUI.
+ * @author Oliver Coates
+ *
+ */
+public class RandomEventGUIInfo {
+	
+	/**
+	 *  The name of this event
+	 */
+	public String name;
+	
+	/**
+	 *  The description of the event
+	 */
+	public String description;
+	
+	/**
+	 *  The effects of the event, presented as a manually written string
+	 */
+	public String effectString;
+	
+	/**
+	 * Constructor method
+	 * @param name Name of the event
+	 * @param description Description of the event
+	 * @param effectString Description of this event's effects
+	 */
+	public RandomEventGUIInfo(String name, String description, String effectString) {
+		this.name = name;
+		this.description = description;
+		this.effectString = effectString;
+	}
+}
