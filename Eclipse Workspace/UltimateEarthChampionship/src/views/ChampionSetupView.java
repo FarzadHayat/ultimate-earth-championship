e7965754ff17ece4002aca684ac9f494aac34f20
Farzad Hayatbakhsh <fha62@uclive.ac.nz>
2023-04-26 17:26:06 +1200

    Extracted the different parts of ChampionSetupView into individual methods.

--- Eclipse Workspace/UltimateEarthChampionship/src/views/ChampionSetupView.java
+++ Eclipse Workspace/UltimateEarthChampionship/src/views/ChampionSetupView.java
@@ -2,16 +2,22 @@ package views;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import champions.AdamSmith;
 import manager.GameManager;
 import model.Champion;
 import model.Purchasable;
 import model.Team;
+import model.Configuration;
 import views.PurchasableCard.CardType;
 
 public class ChampionSetupView extends JPanel {
@@ -26,7 +32,14 @@ public class ChampionSetupView extends JPanel {
 	public ChampionSetupView(Team enemyTeam) {
 		setLayout(new BorderLayout());
 		
-		JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		addBackButton();
+		addChampionsPanel();
+		addChosenChampionsPanel();
+		addNextButton();
+	}
+
+	private void addBackButton() {
+		JPanel backButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JButton backButton = new JButton("Go back");
 		backButton.addActionListener(new ActionListener() {
 			
@@ -36,17 +49,30 @@ public class ChampionSetupView extends JPanel {
 				gameManager.visitStadium();
 			}
 		});
-		northPanel.add(backButton);
-		add(northPanel, BorderLayout.NORTH);
-		
-		JPanel centerPanel = new JPanel();
-		add(centerPanel, BorderLayout.CENTER);
+		backButtonPanel.add(backButton);
+		add(backButtonPanel, BorderLayout.NORTH);
+	}
+
+	private void addChampionsPanel() {
+		JPanel championsPanel = new JPanel(new FlowLayout());
+		add(championsPanel, BorderLayout.WEST);
 		for (Champion champion : gameManager.getPlayerTeam().getChampions()) {
 			DraggablePurchasableCard card = new DraggablePurchasableCard((Purchasable) champion, CardType.MINIMAL);
-			centerPanel.add(card);
+			championsPanel.add(card);
 		}
+	}
+	
+	private void addChosenChampionsPanel() {
+		JPanel chosenChampionsPanel = new JPanel(new GridLayout(Configuration.getInstance().NUM_CHOSEN_CHAMPIONS, 1));
+		add(chosenChampionsPanel, BorderLayout.EAST);
+		for (Champion champion : gameManager.getPlayerTeam().getChosenChampions()) {
+			DraggablePurchasableCard card = new DraggablePurchasableCard((Purchasable) champion, CardType.MINIMAL);
+			chosenChampionsPanel.add(card);
+		}
+	}
 		
-		JPanel southPanel = new JPanel();
+	private void addNextButton() {
+		JPanel nextButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		JButton startButton = new JButton("Start");
 		startButton.addActionListener(new ActionListener() {
 			
@@ -55,8 +81,8 @@ public class ChampionSetupView extends JPanel {
 				gameManager.closeMatchSetup();
 			}
 		});
-		southPanel.add(startButton);
-		add(southPanel, BorderLayout.SOUTH);
+		nextButtonPanel.add(startButton);
+		add(nextButtonPanel, BorderLayout.SOUTH);
 	}
 
 }
