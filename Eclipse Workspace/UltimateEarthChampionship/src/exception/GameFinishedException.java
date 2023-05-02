b22235817e83804648972f651981ec1642ff7c10
Farzad Hayatbakhsh <fha62@uclive.ac.nz>
2023-05-02 12:07:23 +1200

    Added GameFinishedException class

---
+++ Eclipse Workspace/UltimateEarthChampionship/src/exception/GameFinishedException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class GameFinishedException extends Exception {
+
+	private static final long serialVersionUID = -128459909315038105L;
+
+	public GameFinishedException() {
+		super();
+	}
+	
+	public GameFinishedException(String message) {
+		super(message);
+	} 
+}
